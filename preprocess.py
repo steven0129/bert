3a45400ddbe13250a4d2b2e66ebaa3448a69ad63
steven0129 <h3584935@gmail.com>
2019-01-28 01:29:30 +0800

    Fix dataset

--- preprocess.py
+++ preprocess.py
@@ -13,10 +13,11 @@ def addKey(key):
     except:
         hashTable[key] = 1
 
-with open('pair-lcstcs100000+origin.csv') as PAIR:
-    addKey('<UNK>')
+print('前處理詞頻...')
 
+with open('pair.csv') as PAIR:
     for line in tqdm(PAIR):
+        tqdm.write(line[:10])
         [text, summary] = line.split(',')
         addKey('<SOS>')
         addKey('<EOS>')
@@ -39,3 +40,32 @@ with open('pair-lcstcs100000+origin.csv') as PAIR:
     with open('bert-model/TF.csv', 'w') as TF:
         for text, count in tqdm(counts):
             TF.write(f'{text},{count}\n')
+
+print('前處理FastText...')
+
+with open('pair.csv') as PAIR:
+    for line in tqdm(PAIR):
+        tqdm.write(line[:10])
+        [text, summary] = line.split(',')
+
+        with open('bert-model/sents.txt', 'a') as OUT:
+            OUT.write('<SOS> ')
+            
+            paras = text.strip().split('<newline>')
+            for para in paras:
+                for word in jieba.cut(para):
+                    OUT.write(word + ' ')
+
+                if len(paras) != 1: OUT.write('<newline> ')
+
+            OUT.write('<EOS>\n')
+            OUT.write('<SOS> ')
+            for word in jieba.cut(summary.strip()):
+                OUT.write(word + ' ')
+
+            OUT.write('<EOS> ')
+
+            for i in range(len(text) - len(summary)):
+                OUT.write('<PAD> ')
+
+            OUT.write('\n')
