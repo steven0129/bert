5684c0ebabf4a2fbbd7331ee310a1e01eea1b59d
steven0129 <h3584935@gmail.com>
2019-04-01 21:01:09 +0800

    Remove TextCNN

--- modeling.py
+++ modeling.py
@@ -22,39 +22,6 @@ class LanGen(nn.Module):
         out = self.adaptive_softmax.log_prob(encoder_layers[-1].view(-1, 768))
         return out, encoder_layers
 
-class TextCNN(nn.Module):
-    def __init__(self, embedding_dimension):
-        super(TextCNN, self).__init__()
-        self.conv3 = nn.Conv2d(1, 1, (3, embedding_dimension))
-        self.conv4 = nn.Conv2d(1, 1, (4, embedding_dimension))
-        self.conv5 = nn.Conv2d(1, 1, (5, embedding_dimension))
-        self.Max3_pool = nn.MaxPool2d((512-3+1, 1))
-        self.Max4_pool = nn.MaxPool2d((512-4+1, 1))
-        self.Max5_pool = nn.MaxPool2d((512-5+1, 1))
-        self.linear1 = nn.Linear(3, 2)
-
-    def forward(self, x):
-        batch = x.shape[0]
-        # Convolution
-        x1 = F.relu(self.conv3(x))
-        x2 = F.relu(self.conv4(x))
-        x3 = F.relu(self.conv5(x))
-
-        # Pooling
-        x1 = self.Max3_pool(x1)
-        x2 = self.Max4_pool(x2)
-        x3 = self.Max5_pool(x3)
-
-        # capture and concatenate the features
-        x = torch.cat((x1, x2, x3), -1)
-        x = x.view(batch, 1, -1)
-
-        # project the features to the labels
-        x = self.linear1(x)
-        x = x.view(-1, 2)
-
-        return x
-
 class LabelSmoothing(nn.Module):
     "Implement label smoothing."
     def __init__(self, size, padding_idx, smoothing=0.0):
